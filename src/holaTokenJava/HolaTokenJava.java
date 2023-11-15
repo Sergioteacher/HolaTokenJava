2e1862e68474eb4a8f64620bda901e2f3cc2c9a3
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-15 11:30:06 +0100

    Inicio del proyecto

---
+++ src/holaTokenJava/HolaTokenJava.java
@@ -0,0 +1,12 @@
+package holaTokenJava;
+
+public class HolaTokenJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Token");
+		System.out.println("probando clonar a GitHub");
+		System.out.println("usando un Token para conectar con el repositorio remoto");
+	}
+
+}
